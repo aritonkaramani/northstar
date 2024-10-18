47962c53e636cdbf2a6ec88b908e2e0a9589aaaf
Ariton Karamani <aritonkaramani@iMacsomorAriton.lan>
2024-10-19 00:35:51 +0200

    chore: fix

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -2,44 +2,42 @@
   <div>
     <h2>Recruitment</h2>
     <div>
-      Open for every role and class - even if your experience sucks but you
-      think you're good then please apply, on the extra lookout for these
-      specific classes below
-      <ul>
-        <li>Melee flex</li>
-        <li>DK</li>
-        <li>Boomkin</li>
-        <li>Mage</li>
-      </ul>
+      4 days - 20-23
+      Wed, Thurs, Sun, Mon
+
+      On farm we typically raid Sunday
+
+      Recruitment
+      Warrior, Melee flex, Enhancement, Healer, Evoker dps
+      Even if your class isn't listed above and your experience sucks but you think you're good then please apply :)
     </div>
 
     <h2>History</h2>
     <p>
-      Northstar has been around in a semi-hardcore manner since the burning
-      crusade days with various realm rankings over the years. Entering legion a
-      couple of old Northstar players and some others decided to tone it down a
-      bit and not raid as often anymore. However, most people might relate
-      casual raiding or three day raiding as non-progress aimed timewaste;
-      instead, we're looking to push content as much as we did before with full
-      focus at the times we raid while having a big banter doing it.
+      Northstar has been around in a semi-hardcore manner since the burning crusade days with various realm rankings
+      over the
+      years. Entering legion a couple of old Northstar players and some others decided to tone it down a bit and not
+      raid as
+      often anymore. However, most people might relate casual raiding or three day raiding as non-progress aimed
+      timewaste;
+      instead, we’re looking to push content as much as we did before with full focus at the times we raid while having
+      a big
+      banter doing it.
     </p>
     <p>
-      We offer you an international raiding community with progress hungry
-      people who enjoy the raiding phenomena just as much as you do with a
-      “casual” yet focused approach of only four raid days a week with
-      <b>NO SPLITS</b> on Wednesdays,Thursdays, Sundays and Mondays from
-      20:00-23:00 ST. You will be able to see end bosses and progress in mythic
-      modes without having to sacrifice too many other things, we hope this will
-      attract some old hardcore players like some of us to join our ranks. We
-      also tend to raid only 1 day a week on farm and that is on Sundays.
+      We offer you an international raiding community with progress hungry people with a “casual” yet focused approach
+      of only four raid days a week. You will be able to see end bosses and progress in early mythic without having to
+      sacrifice too many other things, we hope this will attract some old hardcore players like some of us to join our
+      ranks.
     </p>
     <p>
-      In return we need you to be a experienced and motivated raider who knows
-      your class in and out, who won't waste the little time we have with petty
-      afks and silly whining. We recruit only the best players in order to make
-      up for the loss of 2-3 raid days. We also want mature players who are able
-      to handle progress bumps and constructive criticism without making raids
-      unbearable for everybody else.
+      In return we expect you to be experienced, or very cracked at the game, and motivated to progress raid. Know your
+      class
+      in and out. You're expected to prep and research (typically provided by the guild) and not come into a progress
+      boss
+      clueless. We also want mature players who are able to communicate their mistakes and/or confusion, take
+      constructive
+      criticism without getting tilted and/or it negatively impacting their play.
     </p>
     <div>
       If this sounds like something you would be interrested in, do not hesitate
@@ -59,3 +57,65 @@ export default {};
 </script>
 
 <style></style>
+
+
+
+4 days - 20-23
+Wed, Thurs, Sun, Mon
+
+On farm we typically raid Sunday
+
+Recruitment
+Warrior, Melee flex, Enhancement, Healer, Evoker dps
+Even if your class isn't listed above and your experience sucks but you think you're good then please apply :)
+
+World rankings in previous tiers
+Amirdrassil #87
+Aberrus #106
+Vault of the Incarnates #96
+
+Sepulcher of the First Ones #187
+Sanctum of Domination #258
+Castle Nathria #279
+
+Ny'alotha The Waking City #2063
+Eternal Palace 8/8M #483
+BoD 9/9M #897
+Uldir 8/8M #1303
+Antorus 9/9M #903
+
+
+History
+Northstar has been around in a semi-hardcore manner since the burning crusade days with various realm rankings over the
+years. Entering legion a couple of old Northstar players and some others decided to tone it down a bit and not raid as
+often anymore. However, most people might relate casual raiding or three day raiding as non-progress aimed timewaste;
+instead, we’re looking to push content as much as we did before with full focus at the times we raid while having a big
+banter doing it.
+
+What do we offer you?
+We offer you an international raiding community with progress hungry people with a “casual” yet focused approach of only
+four raid days a week. You will be able to see end bosses and progress in early mythic without having to sacrifice too
+many other things, we hope this will attract some old hardcore players like some of us to join our ranks.
+
+What do we expect from you?
+In return we expect you to be experienced, or very cracked at the game, and motivated to progress raid. Know your class
+in and out. You're expected to prep and research (typically provided by the guild) and not come into a progress boss
+clueless. We also want mature players who are able to communicate their mistakes and/or confusion, take constructive
+criticism without getting tilted and/or it negatively impacting their play.
+
+Looking forward to hearing from you
+Yours truly, Ari~
+
+Boost
+- ari03024
+
+Contact
+- spdne
+- reminale
+- Dent247
+
+Apply at: northstarguild.com
+
+If you do not hear back from us within 3 days you can consider us not interested
+
+For any questions contact our officers on Discord
--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,48 +1,32 @@
 <template>
   <nav class="sidebar-wrapper">
     <span class="sidebar-title">NORTHSTAR</span>
-    <img src="../assets/Northstarlogo.jpeg" class="northstar-logo" />
+    <img src="../assets/northstar-no-name.png" class="northstar-logo" />
     <div class="external-links">
       <ul>
         <li>
-          <a
-            href="https://raider.io/guilds/eu/ravencrest/northstar"
-            target="_blank"
-          >
+          <a href="https://raider.io/guilds/eu/ravencrest/northstar" target="_blank">
             <img src="../assets/raiderioicon.svg" />
           </a>
         </li>
         <li>
-          <a
-            href="https://www.warcraftlogs.com/guild/eu/ravencrest/northstar"
-            target="_blank"
-          >
+          <a href="https://www.warcraftlogs.com/guild/eu/ravencrest/northstar" target="_blank">
             <img src="../assets/wclogsicon.png" />
           </a>
         </li>
         <li class="wowprogress-icon">
-          <a
-            href="https://www.wowprogress.com/guild/eu/ravencrest/Northstar"
-            target="_blank"
-            >W</a
-          >
+          <a href="https://www.wowprogress.com/guild/eu/ravencrest/Northstar" target="_blank">W</a>
         </li>
       </ul>
     </div>
-    <button>
-      <a
-        href="https://docs.google.com/forms/d/1e8VIW384PbVKMWmp-QBuJ2O7ipDqcNPPfoWqBcD4o9o/"
-        target="_blank"
-      >
+    <a href="https://docs.google.com/forms/d/1e8VIW384PbVKMWmp-QBuJ2O7ipDqcNPPfoWqBcD4o9o/" target="_blank">
+      <button>
         Apply Here
-      </a>
-    </button>
+      </button>
+    </a>
     <div class="nav-links">
       <ul class="link-section">
-        <li
-          :class="currentPage === 1 ? 'active' : ''"
-          @click="$emit('change-page', 1)"
-        >
+        <li :class="currentPage === 1 ? 'active' : ''" @click="$emit('change-page', 1)">
           HOME
         </li>
         <!-- <li
