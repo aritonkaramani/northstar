ee85858573d0354033c9c44fa3fe81104bdacb74
Ariton Karamani <ariton.karamani@gmail.com>
2024-11-18 18:27:43 +0100

    cleanup

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -38,10 +38,8 @@
       If this sounds like something you would be interrested in, do not hesitate
       to add any officer on battletag:
       <ul>
-        <li>Ariton (RL - Boosting) - Ari#2764</li>
-        <li>spdne (Recruitment)</li>
+        <li>Ariton (Recruitment) - Ari#2764</li>
         <li>Dent247 (Recruitment)</li>
-        <li>Reminale (Recruitment)</li>
       </ul>
     </div>
   </div>
@@ -52,32 +50,3 @@ export default {};
 </script>
 
 <style></style>
-
-4 days - 20-23 Wed, Thurs, Sun, Mon On farm we typically raid Sunday Recruitment
-Warrior, Melee flex, Enhancement, Healer, Evoker dps Even if your class isn't
-listed above and your experience sucks but you think you're good then please
-apply :) World rankings in previous tiers Amirdrassil #87 Aberrus #106 Vault of
-the Incarnates #96 Sepulcher of the First Ones #187 Sanctum of Domination #258
-Castle Nathria #279 Ny'alotha The Waking City #2063 Eternal Palace 8/8M #483 BoD
-9/9M #897 Uldir 8/8M #1303 Antorus 9/9M #903 History Northstar has been around
-in a semi-hardcore manner since the burning crusade days with various realm
-rankings over the years. Entering legion a couple of old Northstar players and
-some others decided to tone it down a bit and not raid as often anymore.
-However, most people might relate casual raiding or three day raiding as
-non-progress aimed timewaste; instead, we’re looking to push content as much as
-we did before with full focus at the times we raid while having a big banter
-doing it. What do we offer you? We offer you an international raiding community
-with progress hungry people with a “casual” yet focused approach of only four
-raid days a week. You will be able to see end bosses and progress in early
-mythic without having to sacrifice too many other things, we hope this will
-attract some old hardcore players like some of us to join our ranks. What do we
-expect from you? In return we expect you to be experienced, or very cracked at
-the game, and motivated to progress raid. Know your class in and out. You're
-expected to prep and research (typically provided by the guild) and not come
-into a progress boss clueless. We also want mature players who are able to
-communicate their mistakes and/or confusion, take constructive criticism without
-getting tilted and/or it negatively impacting their play. Looking forward to
-hearing from you Yours truly, Ari~ Boost - ari03024 Contact - spdne - reminale -
-Dent247 Apply at: northstarguild.com If you do not hear back from us within 3
-days you can consider us not interested For any questions contact our officers
-on Discord
